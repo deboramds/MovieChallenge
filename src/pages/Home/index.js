8868b196032c46595aa8451b2e243a76bf67e451
deboramds <deborah.mart@hotmail.com>
2023-07-28 15:54:46 -0300

    estilizaçao-form-login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,46 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthGoogleContext } from '../../contexts/authGoogle';
 import { getMoviesAndSeries } from '../../API/api';
-import './home.css';
+import './home.css'; // Importe o arquivo CSS com as estilizações
 
 export const Home = () => {
-    const { user, signOutUser } = useContext(AuthGoogleContext);
-    const [moviesAndSeries, setMoviesAndSeries] = useState([]);
+  const { user, signOutUser } = useContext(AuthGoogleContext);
+  const [moviesAndSeries, setMoviesAndSeries] = useState([]);
+  const [selectedPoster, setSelectedPoster] = useState(null); // Estado para controlar o poster clicado
 
-    useEffect(() => {
-        // Função para buscar os filmes e séries da API
-        const fetchMoviesAndSeries = async () => {
-            const data = await getMoviesAndSeries();
-            setMoviesAndSeries(data);
-        };
-
-        fetchMoviesAndSeries();
-    }, []);
-
-    const handleSignOut = () => {
-        signOutUser();
+  useEffect(() => {
+    // Função para buscar os filmes e séries da API
+    const fetchMoviesAndSeries = async () => {
+      const data = await getMoviesAndSeries();
+      setMoviesAndSeries(data);
     };
 
-    return (
-        <div>
-            <button onClick={handleSignOut}>Sair</button>
-            <h1>Bem-vindo {user.displayName}</h1>
-            <div className="poster-container">
-                {moviesAndSeries.map((item) => (
-                    <img
-                        key={item.id}
-                        src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-                        alt={item.title || item.name}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+    fetchMoviesAndSeries();
+  }, []);
+
+  const handleSignOut = () => {
+    signOutUser();
+  };
+
+  const handlePosterClick = (posterId) => {
+    setSelectedPoster(posterId); // Define o ID do poster clicado no estado
+  };
+
+  return (
+    <div>
+      <button onClick={handleSignOut}>Sair</button>
+      <h1>Bem-vindo {user.displayName}</h1>
+      <div className="poster-container">
+        {moviesAndSeries.map((item) => (
+          <img
+            key={item.id}
+            src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+            alt={item.title || item.name}
+            className={selectedPoster === item.id ? 'selected' : ''}
+            onClick={() => handlePosterClick(item.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,50 +3,29 @@ import { AuthGoogleContext } from "../../contexts/authGoogle";
 import { Navigate } from "react-router-dom";
 import './login.css';
 
-
 export const Login = () => {
   const { signInGoogle, signed, user } = useContext(AuthGoogleContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aqui você pode chamar uma função para fazer a autenticação com o e-mail e senha
-    // Por exemplo: signInWithEmail(email, password);
-  };
-
-  if (signed) {
-    return <Navigate to="/home" />;
+  async function loginGoogle() {
+    await signInGoogle();
   }
 
-  return (
-    <div>
-      <button onClick={signInGoogle}>Logar com o Google</button>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">E-mail:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+  if (!signed) {
+    return (
+   <div className="container-init">
+      <div className="login-container">
+        <div className="card">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Senha" /> 
+          <button className="buttonEnter">Entrar</button>
+          <div className="buttons-area">
+          <button className="buttonGoogle" onClick={loginGoogle}>Login com o Google</button>
         </div>
-        <div>
-          <label htmlFor="password">Senha:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Entrar</button>
-      </form>
-    </div>
-  );
+      </div>
+      </div>
+   </div>
+    );
+  } else {
+    return <Navigate to="/home" />;
+  }
 };
-
-
